6ff4e7af6a37bbf108e73bf7f47c64d1c1e22063
Ryan Yu <ryan.cao.you@gmail.com>
2013-12-23 22:01:32 -0800

    Finish static pages (home and about)

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+
+  	it "should have the content 'Alpha Kappa Psi'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_content('Alpha Kappa Psi')
+  	end
+  	it "should have the title 'Home'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("UC Berkeley Alpha Kappa Psi | Home")
+  	end
+  end
+
+
+
+  describe "About page" do
+
+  	it "should have the content 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About')
+  	end
+  	it "should have the title 'About Us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("UC Berkeley Alpha Kappa Psi | About Us")
+  	end
+
+  end
+
+end
