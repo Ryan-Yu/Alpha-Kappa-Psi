22c789605fdfd493e0eb9a00643cd59bd5de7f84
Ryan Yu <ryan.cao.you@gmail.com>
2014-01-07 16:37:52 -0800

    Add hostname to prod file (heroku)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ AlphaKappaPsi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  onfig.action_mailer.default_url_options = { :host => 'https://peaceful-lake-3697.herokuapp.com' }
+
 end
