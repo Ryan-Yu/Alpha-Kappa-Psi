0c7ba7a437467381b42d4bdb5ab8be10008ad635
Kevin Xie <xie.kevin11@gmail.com>
2014-01-10 17:15:37 -0800

    Implemented Mailer for Rushee Signup and added PW field

--- app/mailers/active_mailer.rb
+++ app/mailers/active_mailer.rb
@@ -10,20 +10,28 @@ class ActiveMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = root_url
-    mail(to: @user.email, subject: "#{user.name}, Welcome to Alpha Kappa Psi - Alpha Beta Chapter")
+    mail(to: @user.email, subject: "[Alpha Kappa Psi] #{user.name}, Welcome to Alpha Kappa Psi - Alpha Beta Chapter")
   end
 
   #Activation email instance variables
   def activation_email(user)
     @user = user
     @url = root_url
-    mail(to: @user.email, subject: "You've been approved as an active at Alpha Kappa Psi - Alpha Beta Chapter")
+    mail(to: @user.email, subject: "[Alpha Kappa Psi] You've been approved as an active at Alpha Kappa Psi - Alpha Beta Chapter")
   end
 
   #Contact Request Email
   def contact_email(contact_request)
     @contact_request = contact_request
     mail(subject: "[Contact Request] - #{@contact_request.name} - #{@contact_request.subject}", from: @contact_request.email)
+  end
+
+  #Rushee signup email
+  def rushee_signup_email(rushee)
+    @rushee = rushee
+    @url = root_url
+    @rush_events = RushEvent.all
+    mail(to: @rushee.email, subject: "[Alpha Kappa Psi] Thank you for your interest in Alpha Kappa Psi")
   end
 
 end
