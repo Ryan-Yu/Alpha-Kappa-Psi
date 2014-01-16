335c02ac125fbfb70ce3d4004f2e80537c43a23c
Ryan Yu <ryan.cao.you@gmail.com>
2014-01-16 03:13:44 -0800

    Add authenticity token as parameter and removed protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   # Specifies the path that Devise redirects to after active sign-in
   def after_sign_in_path_for(resource)
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   # (via: http://stackoverflow.com/questions/16297797/add-custom-field-column-to-devise-with-rails-4)
 
   def configure_permitted_parameters
-    registration_params = [:name, :email, :major, :pledge_class, :biography, :positions_held, :hometown, :linkedin, :photograph, :display_on_index, :eboard, :password, :password_confirmation]
+    registration_params = [:name, :email, :major, :authenticity_token, :pledge_class, :biography, :positions_held, :hometown, :linkedin, :photograph, :display_on_index, :eboard, :password, :password_confirmation]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) { 
