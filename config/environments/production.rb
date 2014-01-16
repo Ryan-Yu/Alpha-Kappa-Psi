b8d571c11a37485989bddd22a65b298f6025a3fc
Ryan Yu <ryan.cao.you@gmail.com>
2014-01-15 23:14:42 -0800

    Change production heroku URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ AlphaKappaPsi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'https://peaceful-lake-3697.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://calakpsi.herokuapp.com' }
 
 end
