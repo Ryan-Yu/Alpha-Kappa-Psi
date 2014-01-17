7602cc808e017a97a2743b34c491369b34aba251
Kevin Xie <xie.kevin11@gmail.com>
2014-01-17 02:34:49 -0800

    last cache thing for a while

--- app/helpers/actives_helper.rb
+++ app/helpers/actives_helper.rb
@@ -1,2 +1,7 @@
 module ActivesHelper
+  def cache_key_for_actives
+    count          = Active.count
+    max_updated_at = Active.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "active/all-#{count}-#{max_updated_at}"
+  end
 end
