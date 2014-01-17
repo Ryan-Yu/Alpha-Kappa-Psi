62aad16bee33da103056513d7b4f03abdfeb2611
Kevin Xie <xie.kevin11@gmail.com>
2014-01-17 00:20:57 -0800

    improved memcache effectiveness

--- app/helpers/rushees_helper.rb
+++ app/helpers/rushees_helper.rb
@@ -1,2 +1,7 @@
 module RusheesHelper
+  def cache_key_for_rushees
+    count          = Rushee.count
+    max_updated_at = Rushee.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "rushee/all-#{count}-#{max_updated_at}"
+  end
 end
