c6bd4f7bba4fbd66644d12d95fa2f67d0a93b40e
Ryan Yu <ryan.cao.you@gmail.com>
2014-01-24 17:32:41 -0800

    Fix URL and change rush info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ AlphaKappaPsi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'https://calakpsi.com' }
+  config.action_mailer.default_url_options = { :host => 'http://calakpsi.com' }
 
 end
